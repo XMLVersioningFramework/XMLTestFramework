f8a77a14b17c20ba0bf03b05cd008ce0b12defbd
Erik Axelsson <erikaxelsson1@gmail.com>
2014-03-08 11:18:15 +0100

    adding more files

---
+++ app/models/UserStoryHandler.java
@@ -0,0 +1,13 @@
+package models;
+
+public class UserStoryHandler {
+	String baseUrl="../userStorys/";
+	
+	public UserStory loadAllUserStorys(){
+		
+	}
+	
+	public UserStory getAUserStorys(){
+		
+	}
+}
