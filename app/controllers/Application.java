97d12b3571fe688e091947a9e32f93b1e23654ad
Erik Axelsson <erikaxelsson1@gmail.com>
2014-03-08 10:53:50 +0100

    added skeleton

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,13 +7,23 @@ import java.util.*;
 
 public class Application extends Controller {
     
-	public static Result addDataPoint() {
-		return ok(views.html.index.render("Hello my framwork"));
+	public static Result addDataPoint(String i) {
+		return ok(views.html.index.render("Hello my framwork"+i));
 	}
 	
     public static Result index() {
 	List<Task> tasks = Task.find.all();
         return ok(views.html.index.render("Hello my framwork"+tasks.get(0).name));
     }
+    public static Result getUserstories() {
+    	List<Task> tasks = Task.find.all();
+        return ok(views.html.index.render("Hello my framwork"+tasks.get(0).name));
+    }
+    public static Result getTests(int userStoriesId) {
+    	List<Task> tasks = Task.find.all();
+        return ok(views.html.index.render("Hello my framwork"+tasks.get(0).name));
+    }
+        
+    
     
 }
---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,35 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table task (
+  id                        bigint auto_increment not null,
+  name                      varchar(255),
+  done                      tinyint(1) default 0,
+  due_date                  datetime,
+  constraint pk_task primary key (id))
+;
+
+create table test_result (
+  id                        bigint auto_increment not null,
+  value                     bigint,
+  test_name                 varchar(255),
+  back_end                  varchar(255),
+  add_date                  datetime,
+  constraint pk_test_result primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table task;
+
+drop table test_result;
+
+SET FOREIGN_KEY_CHECKS=1;
+
